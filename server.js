ecf75d54827c32face5c14756f9db79379b569e9
root <root@localhost>
2013-04-26 11:54:26 +0000

    .

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var http = require('http');
-var i=0;
 http.createServer(function (req, res) {
-	i++;
-	res.writeHead(200, {
-		'Content-Type': 'text/plain'});
-	res.write('Hello World'+i+req.path+'\n');
-	res.end();
+	var data="";
+	req.on('data', function (d) {
+		data+=d;
+	});
+	req.on('end', function () {
+		console.log(["data",data, req.url]);
+		res.writeHead(200, {
+			'Content-Type': 'text/plain'});
+		res.write("1");
+		res.end();
+	});
 }).listen(9000);
 console.log("started");
